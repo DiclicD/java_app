5b896c77c78aaaccdcea7ebb5d5dfc7e2c455da7
DiclicD <sdjamal756@gmail.com>
2024-10-13 14:22:30 +0200

    version 2

--- Main.java
+++ Main.java
@@ -1,6 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        int y = 3;
+        int x = 5;
+        int z = x + y;
+        System.out.println(z);
+        loop(args);
+        looptwo(args);
+        verificated(args);
     }
+
+    public static void loop(String[] args) {
+       for (int i = 0; i < 10; i++) {
+           System.out.println(i);
+       }
+    }
+
+    public static void looptwo(String[] args) {
+        int x = 0;
+        while (x < 10 ) { 
+            System.out.println(x++);
+        }
+    }
+
+    public static void verificated(String[] args) {
+        int age = 10;
+        if (age >= 18) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    
 
 }
